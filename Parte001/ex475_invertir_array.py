f5f63988a45cdd62724d4343ca226d6d01b7f85a
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-12-28 17:15:54 -0500

    Python - Ejercicio 475
    Python - Ejercicio 475: Invertir el Orden de los Elementos de un Objeto array con el Método reverse().

---
+++ Parte001/ex475_invertir_array.py
@@ -0,0 +1,13 @@
+# Ejercicio 475: Invertir el orden de los elementos de un objeto array.
+
+from array import array
+
+numeros = array('i', [2, 3, 5, 7, 11, 13])
+
+print(numeros)
+
+print()
+
+numeros.reverse()
+
+print(numeros)
