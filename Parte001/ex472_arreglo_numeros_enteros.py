7f7a1572c76569ab3dea72485a6e5214564ea0a1
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-12-28 14:33:38 -0500

    Python - Ejercicio 472
    Python - Ejercicio 472: Crear un Objeto array con Elementos de Tipo Entero.

---
+++ Parte001/ex472_arreglo_numeros_enteros.py
@@ -0,0 +1,12 @@
+# Ejercicio 472: Crear un objeto array con elementos de tipo entero.
+
+from array import array
+
+numeros = array('i', [2, 3, 5, 7])
+
+print(numeros)
+
+print(numeros[0])
+print(numeros[1])
+print(numeros[2])
+print(numeros[3])
