e5c1ab2ece2bba7e4c88f244b38dc9ef57bb6647
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-12-29 17:22:44 -0500

    Python - Ejercicio 500
    Python - Ejercicio 500: Iterar un Ciclo for en Orden Inverso por medio de la Función range().

---
+++ Parte001/ex500_iteracion_for_orden_inverso.py
@@ -0,0 +1,9 @@
+# Ejercicio 500: Iterar un ciclo for en orden inverso por medio de la función range().
+
+for i in range(1, 11):
+    print(i, end=' ')
+
+print()
+
+for i in range(10, 0, -1):
+    print(i, end=' ')
