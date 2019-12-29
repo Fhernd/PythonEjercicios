df910c60416a19a1bd83e6b81521c0783318258b
John Ortiz Ordoñez <johnortizo@outlook.com>
2019-12-29 10:53:22 -0500

    Python - Ejercicio 491
    Python - Ejercicio 491: Crear una Función para Convertir Grados Celsius en Grados Fahrenheit.

---
+++ Parte001/ex491_conversion_celcius_fahrenheit.py
@@ -0,0 +1,10 @@
+# Ejercicio 491: Crear una función para convertir grados Celsius en grados Fahrenheit.
+
+def convertir_celsius_fahrenheit(celsius):
+    """
+    Convierte grados Celsius en grados Fahrenheit.
+    """
+    return (celsius * 9.0/5.0) + 32
+
+
+print(convertir_celsius_fahrenheit(9))
