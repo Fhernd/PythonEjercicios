bfc04e33e2d5c6ad376458339cbcd26b30e6966d
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-16 15:25:39 -0500

    Python - Ejercicio 546
    Python - Ejercicio 546: Crear una Función para Determinar si un Número es Perfecto.

---
+++ Parte001/ex546_funcion_es_numero_perfecto.py
@@ -0,0 +1,20 @@
+# Ejercicio 546: Crear una función para determinar si un número es perfecto.nny
+
+# 6 -> 1, 2, 3 => 1 + 2 + 3 = 6
+# 28 -> 1 + 2 + 4 + 7 + 14 = 28
+
+def es_numero_perfecto(numero):
+    suma = 0
+
+    for i in range(1, numero):
+        if numero % i == 0:
+            suma += i
+    
+    return suma == numero
+
+
+print(es_numero_perfecto(6))
+print(es_numero_perfecto(12)) # 1 + 2 + 3 + 4 + 6 = 16
+print(es_numero_perfecto(28))
+print(es_numero_perfecto(496))
+print(es_numero_perfecto(8128))
