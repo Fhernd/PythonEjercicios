74b33e9ddab40613bfcb5c3d7f442c3c3c67f3f4
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-01-20 09:30:49 -0500

    Python - Ejercicio 609
    Python - Ejercicio 609: Crear una Función para el Algoritmo de Ordenamiento Burbuja sobre una Lista.

---
+++ Parte001/ex609_ordenamiento_burbuja_algoritmo.py
@@ -0,0 +1,16 @@
+# Ejercicio 609: Crear una función para el algoritmo de ordenamiento burbuja sobre una lista.
+
+def ordenamiento_burbuja(lista):
+    for n in range(len(lista) - 1, 0, -1):
+        for i in range(n):
+            if lista[i] > lista[i + 1]:
+                temp = lista[i]
+                lista[i] = lista[i + 1]
+                lista[i + 1] = temp
+
+
+numeros = [7, 19, 2, 13, 5, 23, 29, 11, 17]
+print(numeros)
+
+ordenamiento_burbuja(numeros)
+print(numeros)
