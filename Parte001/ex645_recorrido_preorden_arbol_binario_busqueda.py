9556998435f974a6ed90b8dba989f99c5c824478
infzer0 <johnortizo@outlook.com>
2020-02-19 16:41:35 -0500

    Python - Ejercicio 645
    Python - Ejercicio 645: Crear una Función para Recorrido en Preorden de un Árbol Binario de Búsqueda.

---
+++ Parte001/ex645_recorrido_preorden_arbol_binario_busqueda.py
@@ -0,0 +1,48 @@
+ # Ejercicio 645: Crear función para recorrido preorden de un árbol binario de búsqueda.
+
+class Nodo:
+    def __init__(self, dato):
+        self.izquierda = None
+        self.derecha = None
+        self.dato = dato
+ 
+def insertar(raiz, nodo):
+    if raiz is None:
+        raiz = nodo
+    else:
+        if raiz.dato < nodo.dato:
+            if raiz.derecha is None:
+                raiz.derecha = nodo
+            else:
+                insertar(raiz.derecha, nodo)
+        else:
+            if raiz.izquierda is None:
+                raiz.izquierda = nodo
+            else:
+                insertar(raiz.izquierda, nodo)
+
+def inorden(raiz):
+    if raiz is not None:
+        inorden(raiz.izquierda)
+        print(raiz.dato)
+        inorden(raiz.derecha)
+
+def preorden(raiz):
+    if raiz is not None:
+        print(raiz.dato)
+        preorden(raiz.izquierda)
+        preorden(raiz.derecha)
+
+raiz = Nodo(21)
+insertar(raiz, Nodo(13))
+insertar(raiz, Nodo(10))
+insertar(raiz, Nodo(33))
+insertar(raiz, Nodo(18))
+insertar(raiz, Nodo(25))
+insertar(raiz, Nodo(40))
+
+inorden(raiz)
+
+print()
+
+preorden(raiz)
