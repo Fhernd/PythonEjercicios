250da3e97cddc89bea766a0ce2379b73567c7c10
infzer0 <johnortizo@outlook.com>
2020-02-19 14:51:45 -0500

    Python - Ejercicio 642
    Python - Ejercicio 642: Crear Clase para Representar un Nodo de un Árbol Binario de Búsqueda.

---
+++ Parte001/ex642_nodo_arbol_binario_busqueda.py
@@ -0,0 +1,8 @@
+ # Ejercicio 642: Implementar clase para un nodo de un árbol binario de búsqueda.
+
+class Nodo:
+    def __init__(self, dato):
+        self.izquierda = None
+        self.derecha = None
+        self.dato = dato
+ 
