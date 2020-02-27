38f3d0824334ffbdca5362bac180e5f47a4c0c17
infzer0 <johnortizo@outlook.com>
2020-02-27 07:31:37 -0500

    Python - Ejercicio 695
    Python - Ejercicio 695: Calcular la Edad en Años a partir de la Fecha de Nacimiento.

---
+++ Parte001/ex695_edad_actual_agnios.py
@@ -0,0 +1,14 @@
+# Ejercicio 695: Calcular la edad en años a partir de la fecha de nacimiento.
+
+from datetime import date
+
+def calcular_edad_agnios(fecha_nacimiento):
+    fecha_actual = date.today()
+    resultado = fecha_actual.year - fecha_nacimiento.year
+    resultado -= ((fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+    return resultado
+
+fecha_nacimiento_turin = date(1912, 6, 23)
+edad = calcular_edad_agnios(fecha_nacimiento_turin)
+
+print(f'La edad de Turin es de {edad} años.')
