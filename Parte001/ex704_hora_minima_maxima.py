28bf76af1c0c04a904ed3f8ab91e40393c4b4657
infzer0 <johnortizo@outlook.com>
2020-02-27 17:12:02 -0500

    Python - Ejercicio 704
    Python - Ejercicio 704: Obtener la Hora Mínima y Máxima con las Propiedades min y max de Time.

---
+++ Parte001/ex704_hora_minima_maxima.py
@@ -0,0 +1,9 @@
+# Ejercicio 704: Obtener la hora mínima y máxima con las propiedades min y max de time.
+
+from datetime import time
+
+hora_minima = time.min
+hora_maxima = time.max
+
+print('Hora mínima:', hora_minima)
+print('Hora máxima:', hora_maxima)
