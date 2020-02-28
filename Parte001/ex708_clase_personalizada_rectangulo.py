b8e756ca39874a722b8c55f9e12225242436db37
infzer0 <johnortizo@outlook.com>
2020-02-28 07:30:28 -0500

    Python - Ejercicio 708
    Python - Ejercicio 708: Crear una Clase Personalizada para Operaciones Básicas sobre un Rectángulo.

---
+++ Parte001/ex708_clase_personalizada_rectangulo.py
@@ -0,0 +1,21 @@
+# Ejercicio 708: Crear una clase personalizada para cálculos básicos sobre un rectangulo.
+
+class Rectangulo:
+    def __init__(self, ancho, alto):
+        self.ancho = ancho
+        self.alto = alto
+    
+    def area(self):
+        resultado = self.ancho * self.alto
+
+        return resultado
+    
+    def perimetro(self):
+        resultado = 2 * (self.ancho + self.alto)
+
+        return resultado
+
+
+r = Rectangulo(10, 7)
+print('Área del rectángulo: %.2f' % r.area())
+print('Perímetro del rectángulo: %.2f' % r.perimetro())
