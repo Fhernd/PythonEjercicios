452765fc81462074c2f3dd1277c651c9a81f1bf9
infzer0 <johnortizo@outlook.com>
2020-03-06 15:17:46 -0500

    Python - Ejercicio 750
    Python - Ejercicio 750: Realizar las Operaciones Aritméticas Básicas sobre Números Complejos.

---
+++ Parte001/ex750_aritmetica_numeros_complejos.py
@@ -0,0 +1,16 @@
+# Ejercicio 750: Realizar las operaciones aritméticas básicas sobre números complejos.
+
+numero_1 = 2 + 3j
+numero_2 = -2 -5j
+
+suma = numero_1 + numero_2
+print(suma)
+
+resta = numero_1 - numero_2
+print(resta)
+
+producto = numero_1 * numero_2
+print(producto)
+
+division = numero_1 / numero_2
+print(division)
