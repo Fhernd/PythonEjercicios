64bbd3779d8664a9f3952fd4e91058d13bb1f207
infzer0 <johnortizo@outlook.com>
2020-03-06 07:36:17 -0500

    Python - Ejercicio 743
    Python - Ejercicio 743: Imprimir un Número usando la Coma (,) como Separador de Millares.

---
+++ Parte001/ex743_imprimir_numero_separador_millares.py
@@ -0,0 +1,17 @@
+# Ejercicio 743: Imprimir un número usando la coma (,) como separador de millares.
+
+numero = 1000000
+print(numero)
+print('{:,}'.format(numero))
+
+print()
+
+numero = 1000
+print(numero)
+print('{:,}'.format(numero))
+
+print()
+
+numero = 100
+print(numero)
+print('{:,}'.format(numero))
