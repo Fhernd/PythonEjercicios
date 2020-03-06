aa13bbb66d46faad08359481874cf9281f8a0ba6
infzer0 <johnortizo@outlook.com>
2020-03-06 15:06:22 -0500

    Python - Ejercicio 749
    Python - Ejercicio 749: Obtener la Parte Real y la Parte Imaganiaria de un Número Complejo.

---
+++ Parte001/ex749_partes_numero_complejo.py
@@ -0,0 +1,7 @@
+# Ejercicio 749: Obtener la parte real e imaginaria de un número complejo.
+
+# complejo = complex(5, 3)
+complejo = 5+3j
+print(complejo)
+print(complejo.real)
+print(complejo.imag)
