e06d9c1340761ca120e62fb145a2b12cbe1b5037
infzer0 <johnortizo@outlook.com>
2020-03-07 17:06:40 -0500

    Python - Ejercicio 766
    Python - Ejercicio 766: Tomar de Modo Aleatorio un Elemento de una Colección con la Función choice().

---
+++ Parte001/ex766_seleccion_aleatoria_elemento.py
@@ -0,0 +1,12 @@
+# Ejercicio 766: Tomar de modo aleatorio un elemento de una colección con el método choice.
+
+import random
+from string import ascii_uppercase
+
+numeros = [2, 3, 5, 7, 11, 13, 17, 19]
+
+print(random.choice(numeros))
+
+print(ascii_uppercase)
+
+print(random.choice(ascii_uppercase))
