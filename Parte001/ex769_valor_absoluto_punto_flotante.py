7acd4f1b162b08dbf6563907f03174878b73450b
infzer0 <johnortizo@outlook.com>
2020-05-21 05:48:43 -0500

    Python - Ejercicio 769
    Python - Ejercicio 769: Usar la Función fabs() para Calcular el Valor Absoluto de un Número de Punto Flotante.

---
+++ Parte001/ex769_valor_absoluto_punto_flotante.py
@@ -0,0 +1,10 @@
+# Ejercicio 769: Usar la función fabs() para calcular el valor absoluto de un número de punto flotante.
+
+# fabs() (módulo math): float absolute
+
+from math import fabs
+
+print(fabs(-3.3))
+print(fabs(13.5))
+print(fabs(0.0))
+print(fabs(-0.0))
