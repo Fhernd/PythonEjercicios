baec74aaac107e2e59a95cf01b29c09a9bc8965a
infzer0 <johnortizo@outlook.com>
2020-05-23 08:38:09 -0500

    Python - Ejercicio 779
    Python - Ejercicio 779: Uso de la Función swapcase() para Alternar Minúsculas y Mayúsculas de un string.

---
+++ Parte001/ex779_funcion_swapcase.py
@@ -0,0 +1,9 @@
+# Ejercicio 779: Uso de la función swapcase() para alternar minúsculas y mayúsculas de un string.
+
+escritor = 'Fyodor Dostoevsky'
+
+print('Contenido de la variable `escritor`:', escritor)
+
+escritor = escritor.swapcase()
+
+print('Contenido de la variable `escritor`:', escritor)
