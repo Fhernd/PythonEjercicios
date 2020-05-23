c60fbd3c941d397e5a47b8604fb055a6604c1881
infzer0 <johnortizo@outlook.com>
2020-05-23 08:09:13 -0500

    Python - Ejercicio 777
    Python - Ejercicio 777: Función istitle() para Validar si una Cadena Está Capitalizada Tipo Título de Libro.

---
+++ Parte001/ex777_funcion_istitle.py
@@ -0,0 +1,25 @@
+# Ejercicio 777: Uso de la función str.istitle() para validar si una cadena está capitalizada tipo título de libro.
+
+# Crimen Y Castigo
+
+titulo_libro = 'Crimen Y Castigo'
+
+print(titulo_libro.istitle())
+
+print()
+
+titulo_libro = 'Fyodor Dostoevsky - Crimen Y Castigo'
+
+print(titulo_libro.istitle())
+
+print()
+
+titulo_libro = 'Memorias del Subsuelo'
+
+print(titulo_libro.istitle())
+
+print()
+
+titulo_libro = 'Memorias Del Subsuelo'
+
+print(titulo_libro.istitle())
