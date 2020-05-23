34624fd6b147a685fe4e701b173b1a3c10cd63e7
infzer0 <johnortizo@outlook.com>
2020-05-23 07:49:03 -0500

    Python - Ejercicio 776
    Python - Ejercicio 776: Uso Función isalnum() para Validar si Hay Números en una Cadena de Caracteres.

---
+++ Parte001/ex776_funcion_isalnum.py
@@ -0,0 +1,17 @@
+# Ejercicio 776: Uso de la función isalnum() para validar si hay números en una cadena de caracteres.
+
+cadena = 'Pythonv381'
+
+print(cadena.isalnum())
+
+print()
+
+cadena = 'Niño123ß'
+
+print()
+
+print(cadena.isalnum())
+
+cadena = 'Vacío123'
+
+print(cadena.isalnum())
