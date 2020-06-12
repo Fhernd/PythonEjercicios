da8b4ff1846176cb84e225ed8c228e3ce974574f
infzer0 <johnortizo@outlook.com>
2020-06-12 17:20:07 -0500

    Python - Ejercicio 817: Copiar un Archivo con la Función copyfile() del Módulo shutil
    Copiar un Archivo con la Función copyfile() del Módulo shutil con el lenguaje de programación Python.

--- Parte001/ex817_copiar_un_archivo.py
+++ Parte001/ex817_copiar_un_archivo.py
@@ -1,1 +1,8 @@
-# Ejercicio 817: 
+# Ejercicio 817: Copiar un archivo con la función copyfile() del módulo shutil.
+
+from shutil import copyfile
+
+archivo_origen = 'Parte001/lorem.txt'
+archivo_destino = 'Parte001/lorem_copia.txt'
+
+copyfile(archivo_origen, archivo_destino)
