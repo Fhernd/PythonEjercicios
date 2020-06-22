b1c223d8ae4bc6f02e3e102c3b1f7d2a4ed83626
infzer0 <johnortizo@outlook.com>
2020-06-22 13:48:23 -0500

    Python - Ejercicio 860: Encontrar Todas las Palabras que Contengan Entre 3 y 5 Letras en un Texto
    Encontrar Todas las Palabras que Contengan Entre 3 y 5 Letras en un Texto con el lenguaje de programación Python.

---
+++ Parte001/ex860_palabras_3_a_5_letras.py
@@ -0,0 +1,16 @@
+# Ejercicio 860: Encontrar todas las palabras que tengan entre 3 y 5 letras en un texto.
+
+import re
+
+extracto = 'Al abrir luna el todo nube, criaturas y acaba los para me recodos que huevos aire, los la el diana ilesa en cosas los. La y ligeros borrachos signos, arcos con pisotean se las no que mudas es. Vilo huido resonancia nino la larga, hule vuelve de la ceniza vuelve resonancia. Los y diminutas helechos mármol que de de manteles me, arroyo el las yo los muertos donde con la de. Estremecidos desgarrados los la fría. Diana diana la arcos las tierra escaleras llanura que desnudo. Me vengo oh luna tierra osos muerte para de. El lentejas de y el...'
+
+patron = r'\b\w{3,5}\b'
+
+resultado = re.findall(patron, extracto)
+
+print(len(resultado))
+
+print()
+
+for p in resultado:
+    print(p)
