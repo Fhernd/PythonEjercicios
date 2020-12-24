ce3e56d455183bc2f2fc668629e5d180e68c86fe
John Ortiz Ordoñez <johnortizo@outlook.com>
2020-12-24 16:58:28 -0500

    Python - Ejercicio 927: Generar Todas las Permutaciones Posibles de n Cantidad de Objetos con permutations()
    Generar Todas las Permutaciones Posibles de n Cantidad de Objetos con permutations() con el lenguaje de programación Python.

---
+++ Parte001/ex927_todas_permutaciones_posibles_n_diferentes_objetos.py
@@ -0,0 +1,13 @@
+# Ejercicio 927: Generar todas las permutaciones posibles de n cantidad de objetos con la función permutations().
+
+from itertools import permutations
+
+print(list(permutations([1])))
+
+print()
+
+print(list(permutations([1, 2])))
+
+print()
+
+print(list(permutations([1, 2, 3])))
