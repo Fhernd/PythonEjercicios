8e6c6998c31cb0cd768088bf0e173c841af89f0c
infzer0 <johnortizo@outlook.com>
2021-02-05 08:54:17 -0500

    Python - Ejercicio 946: Usar la Función insert() para Insertar un Dato en un Posición Arbitraria de una Lista
    Usar la Función insert() para Insertar un Dato en un Posición Arbitraria de una Lista con el lenguaje de programación Python.

---
+++ Parte001/ex946_insert_elemento_posicion_especifica_lista.py
@@ -0,0 +1,24 @@
+# Ejercicio 946: Usar la función insert() para insertar un elemento en una posición específica de una lista.
+
+numeros = [1, 2, 4, 5, 7]
+
+print(numeros)
+print(len(numeros))
+
+print()
+
+print(help(numeros.insert))
+
+print()
+
+numeros.insert(2, 3)
+
+print(numeros)
+print(len(numeros))
+
+print()
+
+numeros.insert(5, 6)
+
+print(numeros)
+print(len(numeros))
