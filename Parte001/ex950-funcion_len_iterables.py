34c1d5f6bfbfbd23b38aeb271a2542bcff5db794
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-02-13 09:01:23 -0500

    Python - Ejercicio 950: Usar la Función len() para Calcular el Número de Elementos de un Iterable
    Usar la Función len() para Calcular el Número de Elementos de un Iterable con el lenguaje de programación Python.

---
+++ Parte001/ex950-funcion_len_iterables.py
@@ -0,0 +1,15 @@
+# Ejercicio 950: Usar la función len() para calcular la longitud sobre diferentes datos iterables.
+
+lenguaje = 'Python'
+primos = [2, 3, 5, 7]
+paises = ('Colombia', 'Perú', 'Boliva')
+capitales = {'Colombia': 'Bogotá', 'Argentina': 'Buenos Aires'}
+
+print(len(lenguaje))    # 6
+print(len(primos))  # 4
+print(len(paises))  # 3
+print(len(capitales)) # 2
+
+print()
+
+# print(len(1000))    # TypeError
