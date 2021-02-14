dde1e4d7959b7f65525a058b7acd7225979264aa
infzer0 <johnortizo@outlook.com>
2021-02-14 10:32:34 -0500

    Python - Ejercicio 951: Realizar las 4 Operaciones Aritméticas Básicas con Números Complejos
    Realizar las 4 Operaciones Aritméticas Básicas con Números Complejos con el lenguaje de programación Python.

---
+++ Parte001/ex951_operaciones_aritmeticas_complejos.py
@@ -0,0 +1,14 @@
+# Ejercicio 951: Realizar las diferentes operaciones aritméticas con números complejos.
+
+numero_complejo_1 = 3 + 2j # complex(3, 2)
+numero_complejo_2 = 5 - 3j # complex(5, -3)
+
+suma = numero_complejo_1 + numero_complejo_2
+resta = numero_complejo_1 - numero_complejo_2
+multiplicacion = numero_complejo_1 * numero_complejo_2
+division = numero_complejo_1 / numero_complejo_2
+
+print(suma) # (8 - 1j)
+print(resta)    # (-2 + 5j)
+print(multiplicacion)
+print(division)
