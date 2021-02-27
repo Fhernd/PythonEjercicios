be1ceb8f37cc76b109feb765891d9b445c4b7a64
infzer0 <johnortizo@outlook.com>
2021-02-27 15:42:12 -0500

    Python - Ejercicio 983: Calcular la Suma de Cada una de las Columnas de una Matriz
    Calcular la Suma de Cada una de las Columnas de una Matriz con el lenguaje de programación Python.

---
+++ Parte001/ex983_suma_por_columnas_matriz.py
@@ -0,0 +1,20 @@
+# Ejercicio 983: Calcular la suma de cada una de las columnas de una matriz.
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+suma = []
+
+# [12, 15, 18]
+
+for i in range(len(matriz)):
+    suma_columna = 0
+    for j in range(len(matriz[0])):
+        suma_columna += matriz[i][j]
+
+    suma.append(suma_columna)
+
+print(suma)
