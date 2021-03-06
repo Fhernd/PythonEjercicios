a9234ec4da41f4662dadaa044cbfc57cea31ea5c
infzer0 <johnortizo@outlook.com>
2021-03-06 09:00:44 -0500

    Python Ejercicio: 1012 Definir las Operaciones Aritméticas Básicas Utilizando Expresiones Lambda
    Definir las Operaciones Aritméticas Básicas Utilizando Expresiones Lambda con el lenguaje de programación Python.

---
+++ Parte002/ex1012_operaciones_aritmeticas_expresiones_lambda.py
@@ -0,0 +1,11 @@
+# Ejercicio 1012: Definir las operaciones aritméticas básicas utilizando expresiones lambda.
+
+sumar = lambda x, y: x + y
+restar = lambda x, y: x - y
+multiplicar = lambda x, y: x * y
+dividir = lambda x, y: x / y
+
+print(sumar(2, 3))  # 5
+print(restar(2, 3)) # -1
+print(multiplicar(2, 3)) # 6
+print(dividir(2, 3)) # ~0.6666
