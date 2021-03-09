8e20e6db6e41a03005c32507a69b097b404818ce
infzer0 <johnortizo@outlook.com>
2021-03-09 14:29:00 -0500

    Python Ejercicio: 1026 Intercambiar entre Minúscula y Mayúscula el Texto de una Cadena con swapcase()
    Intercambiar entre Minúscula y Mayúscula el Texto de una Cadena con swapcase() con el lenguaje de programación Python.

---
+++ Parte002/ex1026_intercambiar_minusculas_mayusculas_texto.py
@@ -0,0 +1,11 @@
+# Ejercicio 1026: Intercambiar entre minúscula y mayúscula los caracteres de un texto con la función swapcase().
+
+# Python => pYTHON
+
+frase = 'Python Es Tremendo'
+print(frase)
+
+print()
+
+resultado = frase.swapcase()
+print(resultado)
