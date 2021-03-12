449f56a018ec9b8259d5b7ed7a740f5c1129de10
infzer0 <johnortizo@outlook.com>
2021-03-12 11:21:52 -0500

    Python Ejercicio: 1038 HackerRank Calcular División Entera y Real de Dos Números, Mostrar Resultados
    HackerRank Calcular División Entera y Real de Dos Números, Mostrar Resultados y el lenguaje de programación Python.

---
+++ Parte002/ex1038_hackerrank_division_entera_real.py
@@ -0,0 +1,24 @@
+# Ejercicio 1038: HackerRank Calcular la división entera y real de dos números, y mostrar resultados.
+
+# URL problema: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=false
+
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+
+# No rounding or formatting is necessary.
+
+# Example
+
+
+# The result of the integer division .
+# The result of the float division is .
+# Print:
+
+# 0
+# 0.6
+
+# ...
+
+
