b65404a6adde5490e41bf66dcd71e54754758709
infzer0 <johnortizo@outlook.com>
2021-03-12 08:54:34 -0500

    Python Ejercicio: 1035 Definir una Jerarquía de Herencia desde la Clase Thread para Crear un Reloj
    Definir una Jerarquía de Herencia desde la Clase Thread para Crear un Reloj con el lenguaje de programación Python.

---
+++ Parte002/ex1035_clase_herencia_thread_reloj.py
@@ -0,0 +1,26 @@
+# Ejerciciio 1035: Definir una jerarquía de herencia a partir de la clase Thread para crear un reloj.
+
+import datetime
+import threading
+import sys
+import time
+
+class Reloj(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        while True:
+            tiempo_transcurrido = time.time()
+            fecha = datetime.datetime.fromtimestamp(tiempo_transcurrido)
+            hora_formateada = fecha.strftime('%H:%M:%S')
+            print(hora_formateada)
+
+            sys.stdout.flush()
+
+            time.sleep(1)
+
+
+reloj = Reloj()
+reloj.start()
