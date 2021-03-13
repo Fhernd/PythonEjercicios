b6b894dd209ac8644ea6d12b581d746afbf3a70f
infzer0 <johnortizo@outlook.com>
2021-03-13 10:00:07 -0500

    Python Ejercicio: 1040 HackerRank Impresión de un Mensaje en la Salida Estándar (Pantalla)
    HackerRank Impresión de un Mensaje en la Salida Estándar (Pantalla) con el lenguaje de programación Python.

--- Parte002/ex1039_hackerank_generar_cuadrados_rango.py
+++ Parte002/ex1039_hackerank_generar_cuadrados_rango.py
@@ -1,3 +1,21 @@
 # Ejercicio 1039: HackerRank Generar el cuadrado de los números en el rango de 0 hasta n
 
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+
+# Example
+
+# The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+# 0
+# 1
+# 4
+
+# ...
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i**2)
 
---
+++ Parte002/ex1040_hackerrank_impresion_mensaje_salida_estandar.py
@@ -0,0 +1,12 @@
+# Ejercicio 1040: HackerRank Imprimir en la consola un mensaje de texto con la función print().
+
+# Here is a sample line of code that can be executed in Python:
+
+# print("Hello, World!")
+# You can just as easily store a string as a variable and then print it to stdout:
+
+# my_string = "Hello, World!"
+# print(my_string)
+# The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+print("Hello, World!")
