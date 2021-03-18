2ad39cf5cb35323b3ffcf1bbc933f4413b346b18
infzer0 <johnortizo@outlook.com>
2021-03-18 17:37:13 -0500

    Python Ejercicio: 1063 2/2 HackerRank Verificar Si Ciertas Cadenas Representan Expresiones Regulares
    HackerRank Verificar Si Ciertas Cadenas Representan Expresiones Regulares con el lenguaje de programación Python.

--- Parte002/ex1063_hackerrank_validaciones_cadenas_regex.py
+++ Parte002/ex1063_hackerrank_validaciones_cadenas_regex.py
@@ -1,3 +1,30 @@
 # Ejercicio 1063: HackerRank Verificar si ciertas cadenas representan expresiones regulares.
 
+# You are given a string .
+# Your task is to find out whether  is a valid regex or not.
+
+# Input Format
+
+# The first line contains integer , the number of test cases.
+# The next  lines contains the string .
+
+# ...
+
+import re
+
+def validate_strings_for_regex(string):
+    try:
+        re.compile(string)
+        
+        return True
+    except:
+        return False
+
+
+if __name__ == '__main__':
+    t = int(input())
+    strings = [input() for _ in range(t)]
+    
+    for s in strings:
+        print(validate_strings_for_regex(s))
 
