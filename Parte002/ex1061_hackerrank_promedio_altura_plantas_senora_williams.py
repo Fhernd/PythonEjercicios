b6e28486788864c89a32ec841656d0f84b496bdc
infzer0 <johnortizo@outlook.com>
2021-03-18 09:15:01 -0500

    Python Ejercicio: 1061 2/2 HackerRank Ayudar la Señora Williams a Calcular el Promedio de sus Plantas
    HackerRank Ayudar la Señora Williams a Calcular el Promedio de sus Plantas con el lenguaje de programación Python.

--- Parte002/ex1061_hackerrank_promedio_altura_plantas_senora_williams.py
+++ Parte002/ex1061_hackerrank_promedio_altura_plantas_senora_williams.py
@@ -1,3 +1,26 @@
 # Ejercicio 1061: HackerRank Calcular la altura promedio de las plantas de la señora Williams.
 
+# Task
 
+# Now, let's use our knowledge of sets and help Mickey.
+
+# Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+
+# Formula used:
+
+# ...
+
+def average(heights):
+    heights = set(heights)
+    length = len(heights)
+    
+    result = sum(heights) / length
+    
+    return result
+    
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
