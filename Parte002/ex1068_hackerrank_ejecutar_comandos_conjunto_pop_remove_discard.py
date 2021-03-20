e512a910a51d010994f96c833ac299483595e915
infzer0 <johnortizo@outlook.com>
2021-03-20 09:20:56 -0500

    Python Ejercicio: 1068 2/2 HackerRank Ejecutar los Comandos remove(), discard() y pop() de un Conjunto (set)
    HackerRank Ejecutar los Comandos remove(), discard() y pop() de un Conjunto (set) con el lenguaje de programación Python.

--- Parte002/ex1068_hackerrank_ejecutar_comandos_conjunto_pop_remove_discard.py
+++ Parte002/ex1068_hackerrank_ejecutar_comandos_conjunto_pop_remove_discard.py
@@ -1,3 +1,41 @@
 # Ejercicio 1068: HackerRank Ejecutar los comandos remove(), discard() y pop() sobre un conjunto (set).
 
+# Task
+# You have a non-empty set , and you have to execute  commands given in  lines.
 
+# The commands will be pop, remove and discard.
+
+# ...
+
+def execute_commands(s, commands):
+    for c in commands:
+        parts = c.split()
+        command = parts[0]
+        
+        if command == 'remove':
+            value = int(parts[1])
+            
+            if value in s:
+                s.remove(value)
+        elif command == 'discard':
+            value = int(parts[1])
+            
+            s.discard(value)
+        elif command == 'pop':
+            if len(s):
+                s.pop()
+    
+    return sum(s)
+
+if __name__ == '__main__':
+    n = int(input())
+    s = set(map(int, input().split()))
+    
+    c = int(input())
+    commands = []
+    for i in range(c):
+        commands.append(input())
+    
+    result = execute_commands(s, commands)
+    
+    print(result)
