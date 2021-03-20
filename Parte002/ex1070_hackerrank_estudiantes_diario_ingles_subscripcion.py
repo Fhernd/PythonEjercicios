1b47bad1df5303402796c026dfad20ad128815bd
infzer0 <johnortizo@outlook.com>
2021-03-20 10:20:56 -0500

    Python Ejercicio: 1070 2/2 HackerRank Estudiantes que Están Suscritos a la Versión en Inglés de un Diario
    HackerRank Estudiantes que Están Suscritos a la Versión en Inglés de un Diario con el lenguaje de programación Python.

--- Parte002/ex1070_hackerrank_estudiantes_diario_ingles_subscripcion.py
+++ Parte002/ex1070_hackerrank_estudiantes_diario_ingles_subscripcion.py
@@ -1,3 +1,22 @@
 # Ejercicio 1070: HackerRank Encontrar los estudiantes suscritos a la versión en inglés de un diario.
 
+# Task
+# Students of District College have a subscription to English and French newspapers. Some students have subscribed
+#  to only the English newspaper, some have subscribed to only the French newspaper, and some have subscribed to
+#  both newspapers.
 
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, 
+# and one set has subscribed to the French newspaper. Your task is to find the total number of students 
+# who have subscribed to only English newspapers.
+
+# ...
+
+if __name__ == '__main__':
+    m = int(input())
+    students_english = set(map(int, input().split()))
+    
+    n = int(input())
+    students_french = set(map(int, input().split()))
+    
+    students_english_only = students_english - students_french
+    print(len(students_english_only))
