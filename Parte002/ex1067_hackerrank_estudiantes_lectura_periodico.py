e80dca1ee7735d391ff41f7f14820d50576e6bc2
infzer0 <johnortizo@outlook.com>
2021-03-20 07:20:34 -0500

    Python Ejercicio: 1067 2/2 HackerRank Encontrar Aquellos Estudiantes que Leen al Menos un Diario
    HackerRank Encontrar Aquellos Estudiantes que Leen al Menos un Diario con el lenguaje de programación Python.

--- Parte002/ex1067_hackerrank_estudiantes_lectura_periodico.py
+++ Parte002/ex1067_hackerrank_estudiantes_lectura_periodico.py
@@ -1,3 +1,18 @@
 # Ejercicio 1067: HackerRank Encontrar aquellos estudiantes que leen al menos un diario.
 
+# Task
+# The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
 
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at least one newspaper.
+
+# ...
+
+if __name__ == '__main__':
+    m = int(input())
+    rolls_m = set(list(map(int, input().split())))
+    
+    n = int(input())
+    rolls_n = set(list(map(int, input().split())))
+    
+    result = rolls_m.union(rolls_n)
+    print(len(result))
