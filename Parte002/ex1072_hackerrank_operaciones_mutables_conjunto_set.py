035990114b75e4167e3832a7efd1fb2cda6e4c62
infzer0 <johnortizo@outlook.com>
2021-03-21 10:53:00 -0500

    Python Ejercicio: 1072 3/3 HackerRank Operaciones que Mutan o Cambian un Objeto Conjunto (Set)
    HackerRank Operaciones que Mutan o Cambian un Objeto Conjunto (Set) con el lenguaje de programación Python.

--- Parte002/ex1072_hackerrank_operaciones_mutables_conjunto_set.py
+++ Parte002/ex1072_hackerrank_operaciones_mutables_conjunto_set.py
@@ -1,5 +1,11 @@
 # Ejercicio 1072: HackerRank Operaciones que mutan o cambian un objeto conjunto (set).
 
+# TASK
+# You are given a set  and  number of other sets. These  number of sets have to perform some specific mutation
+#  operations on set .
+
+# Your task is to execute those operations and print the sum of elements from set .
+
 if __name__ == '__main__':
     m = int(input())
     a = set(map(int, input().split()))
