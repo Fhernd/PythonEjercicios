e24a44bda47ba7767dedb3f1244bf4c5ad5c3235
infzer0 <johnortizo@outlook.com>
2021-03-21 10:07:51 -0500

    Python Ejercicio: 1071 2/2 HackerRank Estudiantes Únicamente Suscriptos a la Edición en Inglés y Francés
    HackerRank Estudiantes Únicamente Suscriptos a la Edición en Inglés y Francés con el lenguaje de programación Python.

--- Parte002/ex1071_hackerrank_estudiantes_diario_diferencia_simetrica.py
+++ Parte002/ex1071_hackerrank_estudiantes_diario_diferencia_simetrica.py
@@ -1,3 +1,22 @@
 # Ejercicio 1071: HackerRank Estudiantes únicamennte suscritos a la edición en inlgés y francés de un diario.
 
+# Task
+# Students of District College have subscriptions to English and French newspapers. Some students have subscribed
+#  to English only, some have subscribed to French only, and some have subscribed to both newspapers.
 
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set 
+# has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed 
+# to either the English or the French newspaper but not both.
+
+# ...
+
+if __name__ == '__main__':
+    m = int(input())
+    english_students = set(map(int, input().split()))
+    
+    n = int(input())
+    french_students = set(map(int, input().split()))
+    
+    s = english_students.symmetric_difference(french_students)
+    
+    print(len(s))
