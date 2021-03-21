ac57cb2fe915d9e14f64451eec7738076c83fa91
infzer0 <johnortizo@outlook.com>
2021-03-21 12:30:14 -0500

    Python Ejercicio: 1074 2/2 HackerRank Determinar Si un Conjunto A es Subconjunto de un Conjunto B
    HackerRank Determinar Si un Conjunto A es Subconjunto de un Conjunto B con el lenguaje de programación Python.

--- Parte002/ex1074_hackerrank_verificar_subconjunto_funcion.py
+++ Parte002/ex1074_hackerrank_verificar_subconjunto_funcion.py
@@ -1,9 +1,32 @@
 # Ejercicio 1074: HackerRank Determianr si un conjunto A es subconjunto de un conjunto B.
 
+# You are given two sets,  and .
+# Your job is to find whether set  is a subset of set .
+
+# If set  is subset of set , print True.
+# If set  is not a subset of set , print False.
+
+# ...
+
+def is_subset(a, b):
+    if len(a) > len(b):
+        return False
+    
+    return len(a.difference(b)) == 0
+
 if __name__ == '__main__':
     t = int(input())
     
     sets = []
     
     for i in range(t):
-        pass
+        m = int(input())
+        a = set(map(int, input().split()))
+        
+        n = int(input())
+        b = set(map(int, input().split()))
+        
+        sets.append((a, b))
+    
+    for s in sets:
+        print(is_subset(s[0], s[1]))
