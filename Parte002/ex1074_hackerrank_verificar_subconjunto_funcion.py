cb977b44b4acc075bf7e40720c22fc9e5b692113
infzer0 <johnortizo@outlook.com>
2021-03-21 12:12:35 -0500

    Python Ejercicio: 1074 1/2 HackerRank Determinar Si un Conjunto A es Subconjunto de un Conjunto B
    HackerRank Determinar Si un Conjunto A es Subconjunto de un Conjunto B con el lenguaje de programación Python.

---
+++ Parte002/ex1074_hackerrank_verificar_subconjunto_funcion.py
@@ -0,0 +1,9 @@
+# Ejercicio 1074: HackerRank Determianr si un conjunto A es subconjunto de un conjunto B.
+
+if __name__ == '__main__':
+    t = int(input())
+    
+    sets = []
+    
+    for i in range(t):
+        pass
