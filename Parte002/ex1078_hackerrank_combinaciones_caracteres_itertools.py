e1e9abc4095f1dc44e843c22f061b138cb6dc5bc
infzer0 <johnortizo@outlook.com>
2021-03-23 14:34:07 -0500

    Python Ejercicio: 1078 2/2 HackerRank Imprimir en Orden Lexicográfico las Combinaciones de un Texto
    HackerRank Imprimir en Orden Lexicográfico las Combinaciones de un Texto con el lenguaje de programación Python.

--- Parte002/ex1078_hackerrank_combinaciones_caracteres_itertools.py
+++ Parte002/ex1078_hackerrank_combinaciones_caracteres_itertools.py
@@ -1,3 +1,27 @@
 # Ejercicio 1078: HackerRank Imprimir en orden lexicográfico las combinaciones de varios caracteres.
 
+# Task
 
+# You are given a string .
+# Your task is to print all possible combinations, up to size , of the string in lexicographic sorted order.
+
+from itertools import combinations
+
+if __name__ == '__main__':
+    s, k = input().split()
+    
+    k = int(k)
+    
+    result = []
+    
+    s = sorted(s)
+    
+    for r in range(1, k + 1):
+        combinations_r = list(combinations(s, r))
+        combinations_r = [''.join(c) for c in combinations_r]
+        combinations_r = sorted(combinations_r)
+        
+        result.extend(combinations_r)
+    
+    for c in result:
+        print(c)
