8182418cfcfeb2a89061551328ae68cfb48683cc
infzer0 <johnortizo@outlook.com>
2021-03-22 19:37:13 -0500

    Python Ejercicio: 1076 2/2 HackerRank Producto Cartesiano entre Dos Listas/Tuplas con la Función product()
    Producto Cartesiano entre Dos Listas/Tuplas con la Función product() con el lenguaje de programación Python.

--- Parte002/ex1076_hackerrank_producto_cartesiano_iterables.py
+++ Parte002/ex1076_hackerrank_producto_cartesiano_iterables.py
@@ -1,3 +1,27 @@
 # Ejercicio 1076: HackerRank Producto cartesiano entre dos listas con la función product.
 
+# Task
 
+# You are given a two lists  and . Your task is to compute their cartesian product X.
+
+# Example
+
+# A = [1, 2]
+# B = [3, 4]
+
+# AxB = [(1, 3), (1, 4), (2, 3), (2, 4)]
+# Note:  and  are sorted lists, and the cartesian product's tuples should be output in sorted order.
+
+# ...
+
+from itertools import product
+
+if __name__ == '__main__':
+    a = list(map(int, input().split()))
+    
+    b = list(map(int, input().split()))
+    
+    cartesian_product = list(product(a, b))
+    
+    for c in cartesian_product:
+        print(c, end=' ')
