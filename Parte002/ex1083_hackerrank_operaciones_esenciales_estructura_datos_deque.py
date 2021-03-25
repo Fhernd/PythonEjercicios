2b65544f3ca16add1bc24f7001ad86746ac04e79
infzer0 <johnortizo@outlook.com>
2021-03-24 19:08:48 -0500

    Python Ejercicio: 1083 HackerRank Operaciones Esenciales con la Estructura de Datos (colección) deque
    HackerRank Operaciones Esenciales con la Estructura de Datos (colección) deque con el lenguaje de programación Python.

---
+++ Parte002/ex1083_hackerrank_operaciones_esenciales_estructura_datos_deque.py
@@ -0,0 +1,14 @@
+# Ejercicio 1083: HackerRank Realizar operaciones esenciales sobre un objeto deque (estructura de datos).
+
+# Task
+
+# Perform append, pop, popleft and appendleft methods on an empty deque .
+
+# Input Format
+
+# The first line contains an integer , the number of operations.
+# The next  lines contains the space separated names of methods and their values.
+
+# ...
+
+
