23890115d84e010e8ffbb52d77482381a9e53874
infzer0 <johnortizo@outlook.com>
2021-03-26 18:26:55 -0500

    Python Ejercicio: 1089 2/2 HackerRank Cómputo Rendimiento N Estudiantes de la Universidad Nacional
    HackerRank Cómputo Rendimiento N Estudiantes de la Universidad Nacional con el lenguaje de programación Python.

--- Parte002/ex1089_hackerrank_rendimiento_examinacion_universidad_nacional.py
+++ Parte002/ex1089_hackerrank_rendimiento_examinacion_universidad_nacional.py
@@ -6,3 +6,16 @@
 # Your task is to compute the average scores of each student.
 
 # ...
+
+if __name__ == '__main__':
+    n, x = tuple(map(int, input().split()))
+    
+    scores = []
+    for _ in range(x):
+        scores.append(tuple(map(float, input().split())))
+    
+    result = list(zip(*scores))
+    
+    for r in result:
+        average = sum(r) / len(r)
+        print(average)
