bc3b937cc568907526352e4b8995353ec2b0f569
infzer0 <johnortizo@outlook.com>
2021-03-26 10:26:13 -0500

    Python Ejercicio: 1087 HackerRank Números Enteros Extremadamente Grandes en Python
    HackerRank Números Enteros Extremadamente Grandes en Python.

---
+++ Parte002/ex1087_hackerrank_enteros_muy_grandes.py
@@ -0,0 +1,19 @@
+# Ejercicio 1087: HackerRank Números enteros extremadamente grandes en Python.
+
+# Task
+# Read four numbers, , , , and , and print the result of .
+
+# Input Format
+# Integers , , , and  are given on four separate lines, respectively.
+
+# ...
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    
+    result = a**b + c**d
+    
+    print(result)
