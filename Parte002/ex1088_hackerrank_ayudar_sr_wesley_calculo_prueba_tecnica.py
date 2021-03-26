c74bdf62e456a9eb49fa65bba624c226810b2fbe
infzer0 <johnortizo@outlook.com>
2021-03-26 11:13:25 -0500

    Python Ejercicio: 1088 1/2 HackerRank Ayudar al Sr. Wesley a Calcular Promedios de una Prueba Técnica
    HackerRank Ayudar al Sr. Wesley a Calcular Promedios de una Prueba Técnica con el lenguaje de programación Python.

---
+++ Parte002/ex1088_hackerrank_ayudar_sr_wesley_calculo_prueba_tecnica.py
@@ -0,0 +1,14 @@
+# Ejercicio 1088: HackerRank Ayudar al sr. Wesley a caulcular los promedios de una prueba técnica.
+
+# Task
+
+# Dr. John Wesley has a spreadsheet containing a list of student's , ,  and .
+
+# Your task is to help Dr. Wesley calculate the average marks of the students.
+
+
+# Note:
+# 1. Columns can be in any order. IDs, marks, class and name can be written in any order in the spreadsheet.
+# 2. Column names are ID, MARKS, CLASS and NAME. (The spelling and case type of these names won't change.)
+
+# ...
