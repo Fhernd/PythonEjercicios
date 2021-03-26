ff6cc79618225617b7639252a23a6fd4746594b1
infzer0 <johnortizo@outlook.com>
2021-03-26 18:12:04 -0500

    Python Ejercicio: 1089 1/2 HackerRank Cómputo Rendimiento N Estudiantes de la Universidad Nacional
    HackerRank Cómputo Rendimiento N Estudiantes de la Universidad Nacional con el lenguaje de programación Python.

---
+++ Parte002/ex1089_hackerrank_rendimiento_examinacion_universidad_nacional.py
@@ -0,0 +1,8 @@
+# Ejercicio 1089: HackerRank Cómputo rendimiento N estudiantes de la Universidad Nacional.
+
+# Task
+
+# The National University conducts an examination of  students in  subjects.
+# Your task is to compute the average scores of each student.
+
+# ...
