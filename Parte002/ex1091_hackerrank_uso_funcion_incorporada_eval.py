377cc9c91085c165d6d7d285693002c28d424703
infzer0 <johnortizo@outlook.com>
2021-03-27 16:27:52 -0500

    Python Ejercicio: 1091 HackerRank Utilizar la Función Incorporada eval() para Evaluar Expresión Python
    HackerRank Utilizar la Función Incorporada eval() para Evaluar Expresión Python con el lenguaje de programación Python.

---
+++ Parte002/ex1091_hackerrank_uso_funcion_incorporada_eval.py
@@ -0,0 +1,10 @@
+# Ejercicio 1091: HackerRank Utilizar la función incorporada eval() para evaluar una expresión Python.
+
+# Task
+# You are given an expression in a line. Read that line as a string variable, such as var, and 
+# print the result using eval(var).
+
+if __name__ == '__main__':
+    expresion = input()
+    
+    eval(expresion)
