e890565be92146d699368ba5318484abb8cd820c
infzer0 <johnortizo@outlook.com>
2021-03-30 11:08:45 -0500

    Python Ejercicio: 1097 1/2 HackerRank Reconocimiento de Grupos y Subgrupos con un Patrón de RegEx
    HackerRank Reconocimiento de Grupos y Subgrupos con un Patrón de RegEx con el lenguaje de programación Python.

---
+++ Parte002/ex1097_hackerrank_grupos_subgrupos_match_regex.py
@@ -0,0 +1,10 @@
+# Ejercicio 1097: HackerRank Reconocimiento de grupos y subgrupos con un patrón de expresión regular.
+
+# Task
+
+# You are given a string .
+
+# Your task is to find the first occurrence of an alphanumeric character in  (read from left to right)
+# that has consecutive repetitions.
+
+
