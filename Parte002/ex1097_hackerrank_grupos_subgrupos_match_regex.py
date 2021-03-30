cad202c4f844ee992202b2bb4d4092678f0d3469
infzer0 <johnortizo@outlook.com>
2021-03-30 11:46:33 -0500

    Python Ejercicio: 1097 2/2 HackerRank Reconocimiento de Grupos y Subgrupos con un Patrón de RegEx
    HackerRank Reconocimiento de Grupos y Subgrupos con un Patrón de RegEx con el lenguaje de programación Python.

--- Parte002/ex1097_hackerrank_grupos_subgrupos_match_regex.py
+++ Parte002/ex1097_hackerrank_grupos_subgrupos_match_regex.py
@@ -7,4 +7,16 @@
 # Your task is to find the first occurrence of an alphanumeric character in  (read from left to right)
 # that has consecutive repetitions.
 
+import re
 
+if __name__ == '__main__':
+    s = input()
+    
+    pattern = r'([0-9]|[a-z]|[A-Z])\1'
+    
+    m = re.search(pattern, s)
+    
+    if m:
+        print(m.group()[0])
+    else:
+        print(-1)
