6a517d88ef40c9fb5478637ef8f584ba22063e04
infzer0 <johnortizo@outlook.com>
2021-03-31 15:58:07 -0500

    Python Ejercicio: 1099 HackerRank Propiedad shape y Función reshape() para Cambiar Forma de un Arreglo
    HackerRank Propiedad shape y Función reshape() para Cambiar Forma de un Arreglo con el lenguaje de programación Python.

---
+++ Parte002/ex1099_hackerrank_shape_reshape_forma_arreglo.py
@@ -0,0 +1,10 @@
+# Ejercicio 1099: HackerRank Modificar la forma de un arreglo con la propiedad shape y la función reshape().
+
+# Task
+
+# You are given a space separated list of nine integers. Your task is to convert this list 
+# into a X NumPy array.
+
+# ...
+
+
