83ba52f38d21b3180344dcfed85bf8a997b3aae3
infzer0 <johnortizo@outlook.com>
2021-04-06 15:13:33 -0500

    Python Ejercicio: 1114 2/2 HackerRank Usar una Función Decoradora para Formatear Números de Teléfono
    HackerRank Usar una Función Decoradora para Formatear Números de Teléfono con el lenguaje de programación Python.

--- Parte002/ex1114_hackerrank_funcion_decoradora_numero_telefono.py
+++ Parte002/ex1114_hackerrank_funcion_decoradora_numero_telefono.py
@@ -6,4 +6,17 @@
 
 # ...
 
+def wrapper(f):
+    def fun(l):
+        l = [p[-10:] for p in l]
+        l = ['+91 ' + p[:5] + ' ' + p[-5:] for p in l]
+        f(l)
+    return fun
 
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
