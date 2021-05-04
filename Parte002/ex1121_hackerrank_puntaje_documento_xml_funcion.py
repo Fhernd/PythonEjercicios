bb5480095a5a605cfa1cfd39bb699253ab29e4a0
infzer0 <johnortizo@outlook.com>
2021-05-04 08:40:21 -0500

    Python Ejercicio: 1121 2/2 HackerRank Calcular el Puntaje de un Documento XML Contando sus Atributos
    HackerRank Calcular el Puntaje de un Documento XML Contando sus Atributos con el lenguaje de programación Python.

---
+++ Parte002/ex1121_hackerrank_puntaje_documento_xml_funcion.py
@@ -0,0 +1,27 @@
+# Ejercicio 1121: HackerRank Calcular el puntaje de un documento XML contando el número de atributos de los elementos.
+
+# You are given a valid XML document, and you have to print its score. The score is calculated by the sum of 
+# the score of each element. For any element, the score is equal to the number of attributes it has.
+
+# Input Format
+
+# The first line contains , the number of lines in the XML document.
+# The next  lines follow containing the XML document.
+
+# ...
+
+import sys
+import xml.etree.ElementTree as etree
+
+
+def get_attr_number(node):
+    # your code goes here
+    pass
+
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
