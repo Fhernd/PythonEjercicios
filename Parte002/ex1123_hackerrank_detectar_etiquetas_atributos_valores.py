8629935b60643ea8dc187025c612909cff70359f
infzer0 <johnortizo@outlook.com>
2021-05-08 13:47:09 -0500

    Python Ejercicio: 1123 1/2 HackerRank Extraer las Etiquetas, Atributos y Valores de un Documento HTML
    HackerRank Extraer las Etiquetas, Atributos y Valores de un Documento HTML con el lenguaje de programación Python.

---
+++ Parte002/ex1123_hackerrank_detectar_etiquetas_atributos_valores.py
@@ -0,0 +1,8 @@
+# Ejercicio 1123: HackerRank Extraer las etiquetas, atributos y valores de un documento HTML.
+
+# You are given an HTML code snippet of N lines.
+# Your task is to detect and print all the HTML tags, attributes and attribute values.
+
+# Print the detected items in the following format:
+
+# ...
