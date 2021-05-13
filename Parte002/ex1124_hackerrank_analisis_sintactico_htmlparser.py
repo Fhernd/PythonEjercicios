301deaa9bec48cc30ea73024886405ccac569e3e
infzer0 <johnortizo@outlook.com>
2021-05-13 14:05:00 -0500

    Python Ejercicio: 1024 1/2 HackerRank Realizar Análisis Sintáctico a un Documento HTML con HTMLParser
    HackerRank Realizar Análisis Sintáctico a un Documento HTML con HTMLParser con el lenguaje de programación Python.

---
+++ Parte002/ex1124_hackerrank_analisis_sintactico_htmlparser.py
@@ -0,0 +1,14 @@
+# Ejercicio 1124: HackerRank Realizar análisis sintáctico a un documento HTML con HTMLParser.
+
+# HTML
+# Hypertext Markup Language is a standard markup language used for creating World Wide Web pages.
+
+# Parsing
+# Parsing is the process of syntactic analysis of a string of symbols. It involves resolving a string into 
+# its component parts and describing their syntactic roles.
+
+# HTMLParser
+# An HTMLParser instance is fed HTML data and calls handler methods when start tags, end tags, text, comments, 
+# and other markup elements are encountered.
+
+# ...
