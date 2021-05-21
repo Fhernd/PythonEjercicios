fcedaddbd7e531d2b786c8cddf374c1d973624a7
infzer0 <johnortizo@outlook.com>
2021-05-21 14:22:59 -0500

    Python Ejercicio: 1125 2/2 HackerRank Leer los Comentarios de Única Línea y Multilínea con HTMLParser
    HackerRank Leer los Comentarios de Única Línea y Multilínea con HTMLParser con el lenguaje de programación Python.

--- Parte002/ex1125_hackerrank_leer_html_comentarios_linea_multilinea.py
+++ Parte002/ex1125_hackerrank_leer_html_comentarios_linea_multilinea.py
@@ -8,22 +8,28 @@
 
 from html.parser import HTMLParser
 
+
 class MyHTMLParser(HTMLParser):
-    pass
-  
-  
-  
-  
-  
-  
-  
-  
-  
-html = ""       
-for i in range(int(input())):
-    html += input().rstrip()
-    html += '\n'
-    
-parser = MyHTMLParser()
-parser.feed(html)
-parser.close()
+    def handle_comment(self, data):
+        if len(data):
+            if '\n' in data:
+                print(">>> Multi-line Comment")
+            else:
+                print(">>> Single-line Comment")
+            
+            print(data)
+    def handle_data(self, data):
+        if len(data.strip()):
+            print(">>> Data")
+            print(data)
+
+
+if __name__ == '__main__':
+    html = ""
+    for i in range(int(input())):
+        html += input().rstrip()
+        html += '\n'
+        
+    parser = MyHTMLParser()
+    parser.feed(html)
+    parser.close()
