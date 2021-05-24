cc62fd2eb725610710c963d7dd961e565e97d9ba
infzer0 <johnortizo@outlook.com>
2021-05-24 15:28:53 -0500

    Python Ejercicio: 1126 1/2 HackerRank Validar Si un Texto (String) Representa un Número Romano
    HackerRank Validar Si un Texto (String) Representa un Número Romano con el lenguaje de programación Python.

---
+++ Parte002/ex1126_hackerrank_validar_texto_si_es_numero_romano.py
@@ -0,0 +1,20 @@
+# Ejercicio 1126: HackerRank Validar si un texto representa un número romano.
+
+# You are given a string, and you have to validate whether it's a valid Roman numeral. 
+# If it is valid, print True. Otherwise, print False. Try to create a regular expression 
+# for a valid Roman numeral.
+
+# Input Format
+
+# A single line of input containing a string of Roman characters.
+
+# Output Format
+
+# Output a single line containing True or False according to the instructions above.
+
+# ...
+
+regex_pattern = r""	# Do not delete 'r'.
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
