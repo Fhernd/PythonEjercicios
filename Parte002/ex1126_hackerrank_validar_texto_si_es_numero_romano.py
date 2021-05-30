9506042f3bdd874a58c1957da45f30739b397ba7
infzer0 <johnortizo@outlook.com>
2021-05-30 17:23:15 -0500

    Python Ejercicio: 1126 2/2 HackerRank Validar Si un Texto (String) Representa un Número Romano
    HackerRank Validar Si un Texto (String) Representa un Número Romano con el lenguaje de programación Python.

--- Parte002/ex1126_hackerrank_validar_texto_si_es_numero_romano.py
+++ Parte002/ex1126_hackerrank_validar_texto_si_es_numero_romano.py
@@ -14,7 +14,7 @@
 
 # ...
 
-regex_pattern = r""	# Do not delete 'r'.
+regex_pattern = r"^M{0,3}(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"	# Do not delete 'r'.
 
 import re
 print(str(bool(re.match(regex_pattern, input()))))
