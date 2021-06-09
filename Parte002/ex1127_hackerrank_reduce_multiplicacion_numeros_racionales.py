766d5afd7ad6e45b10576ff6810afa51e91e312c
infzer0 <johnortizo@outlook.com>
2021-06-08 19:02:36 -0500

    Python Ejercicio: 1127 1/2 HackerRank Uso de la Función reduce() para el Producto de Números Racionales
    HackerRank Uso de la Función reduce() para el Producto de Números Racionales con el lenguaje de programación Python.

---
+++ Parte002/ex1127_hackerrank_reduce_multiplicacion_numeros_racionales.py
@@ -0,0 +1,22 @@
+# Ejercicio 1127: HackerRank Multiplicar números racionales por medio de la función reduce() de functools.
+
+# Given a list of rational numbers,find their product.
+
+# Concept
+# The reduce() function applies a function of two arguments cumulatively on a list of objects in 
+# succession from left to right to reduce it to one value. Say you have a list, say [1,2,3] and 
+# you have to find its sum.
+
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = # complete this line with a reduce statement
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
