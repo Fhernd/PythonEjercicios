7527a23c60ebf720b6ff0387c4e16a3b2b801033
infzer0 <johnortizo@outlook.com>
2021-06-21 16:19:21 -0500

    Python Ejercicio: 1130 1/2 HackerRank Encontrar Todas las Subcadenas que Cumplan un Formato Dado
    HackerRank Encontrar Todas las Subcadenas que Cumplan un Formato Dado con el lenguaje de programación Python.

---
+++ Parte002/ex1130_hackerrank_subcadenas_formato_especifico.py
@@ -0,0 +1,18 @@
+# Ejercicio 1130: HackerRank Encontrar todas las subcadenas que cumplan un formato dado.
+
+# Consider the following:
+
+# A string, , of length  where .
+# An integer, , where  is a factor of .
+# We can split  into  substrings where each subtring, , consists of a contiguous block of  characters in . 
+# Then, use each  to create string  such that:
+
+# ...
+
+def merge_the_tools(string, k):
+    # your code goes here
+    pass
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
