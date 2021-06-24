4881aeccc29caaa1cab9f99dbe53dbd2d339b030
infzer0 <johnortizo@outlook.com>
2021-06-24 12:53:21 -0500

    Python Ejercicio: 1131 1/2 HackerRank Uso de Iterables e Iteradores para Combinaciones de Letras
    HackerRank Uso de Iterables e Iteradores para Combinaciones de Letras con el lenguaje de programación Python.

---
+++ Parte002/ex1131_hackerrank_iterables_iteradores_combinaciones_letras.py
@@ -0,0 +1,7 @@
+# Ejercicio 1131: HackerRank Uso de iterables e iteradores para combinaciones índices de letras.
+
+# The itertools module standardizes a core set of fast, memory efficient tools that are useful by 
+# themselves or in combination. Together, they form an iterator algebra making it possible to construct 
+# specialized tools succinctly and efficiently in pure Python.
+
+# To read more about the functions in this module, check out their
