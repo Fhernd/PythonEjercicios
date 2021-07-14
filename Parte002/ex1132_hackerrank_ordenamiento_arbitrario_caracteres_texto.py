9dff0c450eb938f3e33ffe688efae53af6d400a0
infzer0 <johnortizo@outlook.com>
2021-07-14 17:11:09 -0500

    Python Ejercicio: 1132 1/2 HackerRank Crear Ordenamiento Arbitrario de los Caracteres de un Texto
    HackerRank Crear Ordenamiento Arbitrario de los Caracteres de un Texto con el lenguaje de programación Python.

---
+++ Parte002/ex1132_hackerrank_ordenamiento_arbitrario_caracteres_texto.py
@@ -0,0 +1,14 @@
+# Ejercicio 1132: HackerRank Crear ordenamiento arbitrario para los caracteres de un texto.
+
+# You are given a string .
+#  contains alphanumeric characters only.
+#  Your task is to sort the string  in the following manner:
+
+# All sorted lowercase letters are ahead of uppercase letters.
+# All sorted uppercase letters are ahead of digits.
+# All sorted odd digits are ahead of sorted even digits.
+
+if __name__ == '__main__':
+    s = input()
+    
+    print(s)
