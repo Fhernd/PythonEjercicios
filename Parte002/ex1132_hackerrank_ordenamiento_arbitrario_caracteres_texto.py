f2474f93d3f0d704f6833c29b3be8ef479639ace
infzer0 <johnortizo@outlook.com>
2021-07-25 12:12:16 -0500

    Python Ejercicio: 1132 4/2 HackerRank Crear Ordenamiento Arbitrario de los Caracteres de un Texto
    HackerRank Crear Ordenamiento Arbitrario de los Caracteres de un Texto con el lenguaje de programación Python.

--- Parte002/ex1132_hackerrank_ordenamiento_arbitrario_caracteres_texto.py
+++ Parte002/ex1132_hackerrank_ordenamiento_arbitrario_caracteres_texto.py
@@ -1,34 +1,27 @@
-# Ejercicio 1132: HackerRank Crear ordenamiento arbitrario para los caracteres de un texto.
-
-# You are given a string .
-#  contains alphanumeric characters only.
-#  Your task is to sort the string  in the following manner:
-
-# All sorted lowercase letters are ahead of uppercase letters.
-# All sorted uppercase letters are ahead of digits.
-# All sorted odd digits are ahead of sorted even digits.
-
 from string import ascii_lowercase, ascii_uppercase, digits
 
 
 def sort_text(text):
-    minusculas = list(set(text).intersection(set(ascii_lowercase)))
-    mayusculas = list(set(text).intersection(set(ascii_uppercase)))
-    digitos = list(set(text).intersection(set(digits)))
+    if text.isalnum():
+        minusculas = list(filter(lambda c: c in ascii_lowercase, text))
+        mayusculas = list(filter(lambda c: c in ascii_uppercase, text))
+        digitos = list(filter(lambda c: c in digits, text))
+        
+        minusculas = ''.join(sorted(minusculas))
+        mayusculas = ''.join(sorted(mayusculas))
+        digitos = list(map(int, digitos))
+        
+        impares = sorted(list(filter(lambda n: n % 2 == 1, digitos)))
+        pares = sorted(list(filter(lambda n: n % 2 == 0, digitos)))
+        
+        impares = ''.join(str(i) for i in impares)
+        pares = ''.join(str(i) for i in pares)
+        
+        return minusculas + mayusculas + impares + pares
     
-    minusculas = ''.join(sorted(minusculas))
-    mayusculas = ''.join(sorted(mayusculas))
-    digitos = list(map(int, digitos))
-    
-    impares = sorted(list(filter(lambda n: n % 2 == 1, digitos)))
-    pares = sorted(list(filter(lambda n: n % 2 == 0, digitos)))
-    
-    impares = ''.join(str(i) for i in impares)
-    pares = ''.join(str(i) for i in pares)
-    
-    return minusculas + mayusculas + impares + pares
+    return ''
 
-
+    
 if __name__ == '__main__':
     s = input()
     
