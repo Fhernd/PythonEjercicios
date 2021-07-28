c3970aef233d1bf88789800c2714af5d3779b988
infzer0 <johnortizo@outlook.com>
2021-07-28 09:45:48 -0500

    Python Ejercicio: 1133 1/2 HackerRank Ordenar los Registros de una Tabla a partir de una Columa Dada
    HackerRank Ordenar los Registros de una Tabla a partir de una Columa Dada con el lenguaje de programación Python.

---
+++ Parte002/ex1133_hackerrank_ordenar_registros_columna_especifica.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+# You are given a spreadsheet that contains a list of  athletes and their details 
+# (such as age, height, weight and so on). You are required to sort the data based on the th 
+# attribute and print the final resulting table. Follow the example given below for better understanding.
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
